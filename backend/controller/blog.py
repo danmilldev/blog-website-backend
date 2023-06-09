0297b1460c389aefa73bafe34bb469f89b8892aa
danmilldev <126679134+danmilldev@users.noreply.github.com>
2023-06-09 21:52:12 +0200

    changed whole controller to be more valid for a blog website

--- backend/controller/blog.py
+++ backend/controller/blog.py
@@ -1,12 +1,25 @@
 """This file is for all Blog related logic behaviour"""
-from utils.helper import is_a_duplicate
 from flask_restful import Resource, reqparse
 
-tasks = {1: "my new task", 2: "a really nice task", 3: "the best task"}
+posts = {
+    1: {
+        "post_title": "First Blog Post",
+        "post_description": "This is the description of the first blog post.",
+    },
+    2: {
+        "post_title": "Second Blog Post",
+        "post_description": "This is the description of the second blog post.",
+    },
+    3: {
+        "post_title": "Third Blog Post",
+        "post_description": "This is the description of the third blog post.",
+    },
+}
 
 blog_post_args = reqparse.RequestParser()
-blog_post_args.add_argument("task_id", type=int)
-blog_post_args.add_argument("task_name", type=str)
+blog_post_args.add_argument("post_id", type=int)
+blog_post_args.add_argument("post_title", type=str)
+blog_post_args.add_argument("post_description", type=str)
 
 
 class Blog(Resource):
@@ -14,61 +27,65 @@ class Blog(Resource):
 
     def __init__(self):
         self.args = blog_post_args.parse_args()
-        self.task_id = self.args["task_id"]
-        self.task_name = self.args["task_name"]
+        self.post_id = self.args["post_id"]
+        self.post_title = self.args["post_title"]
+        self.post_description = self.args["post_description"]
 
     def get(self):
         """
         This get method route will
         return all tasks stored in the list.
         """
-        return {"tasks": tasks}, 200
+        return {"posts": posts}, 200
 
     def post(self):
         """
         The post method route will if its not a duplicate
         adding the new task send via body to the list.
         """
-        if self.task_name is None:
-            return {"Error": "task_name is required for adding a new task."}, 400
+        if not all([self.post_title, self.post_description]):
+            return {
+                "Error": "You need a post_id, post_title, and post_description to create a post."
+            }, 400
 
-        if is_a_duplicate(item=self.task_id, collection=tasks.keys()):
-            return {"Error": "task_id already in list."}, 400
+        if self.post_id in posts:
+            return {"Error": "Post with that post_id already exists."}, 400
 
-        if is_a_duplicate(item=self.task_name, collection=tasks.values()):
-            return {"Error": "task already in list."}, 400
+        if any(post["post_title"] == self.post_title for post in posts.values()):
+            return {"Error": "Post with that post_title already exists."}, 400
 
-        new_task_id = max(tasks.keys()) + 1
-        tasks[new_task_id] = self.task_name
-        return {"tasks": tasks}, 201
+        new_post_id = max(posts.keys()) + 1
+        posts[new_post_id] = {
+            "post_title": self.post_title,
+            "post_description": self.post_description,
+        }
+        return {"posts": posts}, 201
 
     def put(self):
         """The put method route will update a task."""
-        if is_a_duplicate(item=self.task_id, collection=tasks.keys()):
-            return {"Error": "task_id already in list."}, 400
+        if self.post_id is None:
+            return {
+                "Error": "To change a post you are required to enter a post_id."
+            }, 400
 
-        if is_a_duplicate(item=self.task_name, collection=tasks.values()):
-            return {"Error": "task already in list."}, 400
+        if self.post_id not in posts:
+            return {
+                "Error": f"Post with that post_id: ({self.post_id}) does not exist."
+            }, 400
 
-        if self.task_id is None:
-            return {"Error": "task_id is required for the change of an task."}, 400
-
-        if self.task_name is None:
-            return {"Error": "task_name is required for a new task name."}, 400
-
-        tasks[self.task_id] = self.task_name
-        return {"tasks": tasks}, 201
+        posts[self.post_id] = self.post_title
+        return {"posts": posts}, 201
 
     def delete(self):
         """
         The delete method route will delete a task
         depending if the task_id even exists.
         """
-        if self.task_id is None:
+        if self.post_id is None:
             return {"Error": "task_id is required for removing the task."}, 400
 
-        if self.task_id not in tasks:
+        if self.post_id not in posts:
             return {"Error": "Task with that task_id does not exist."}, 400
 
-        del tasks[self.task_id]
-        return {"tasks": tasks}, 204
+        del posts[self.post_id]
+        return {"posts": posts}, 204
