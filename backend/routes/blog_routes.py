568c474d708df52ed81665996d6a1bd3aed40f0b
danmilldev <126679134+danmilldev@users.noreply.github.com>
2023-06-09 20:17:57 +0200

    added comments to the blog routes file

--- backend/routes/blog_routes.py
+++ backend/routes/blog_routes.py
@@ -1,5 +1,7 @@
+"""This file will be responsible for all route depending for the blog part"""
 from controller.blog import Blog
 
 
 def blog_routes(api):
+    """This method will add the endpoint to the resful api"""
     api.add_resource(Blog, "/blog")
