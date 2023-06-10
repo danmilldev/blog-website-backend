bd4f0c9760dfbfec7c3d7835b3993acb267a7c2a
danmilldev <126679134+danmilldev@users.noreply.github.com>
2023-06-10 12:18:58 +0200

    setup my database connection and .env setup

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from routes.blog_routes import blog_routes
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv(".env")
 
 # flask --app app.py --debug run
 app = Flask(__name__)
 api = Api(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+db = SQLAlchemy(app)
 
 blog_routes(api=api)
 
